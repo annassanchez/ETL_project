3087b67dd4bed4e9d9c2bc8aefc5d9a7ead5a694
annassanchez <annassanchez@gmail.com>
2023-01-17 12:56:05 +0100

    fixing sql info -- there seems to be an error

--- sql_scripts/create.sql
+++ sql_scripts/create.sql
@@ -5,35 +5,53 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema playlists_analysis
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
--- -----------------------------------------------------
--- Schema playlists_analysis
--- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8mb3 ;
 USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`artist`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`artist` (
+  `url` VARCHAR(60) NOT NULL,
+  `artist` VARCHAR(1000) NULL,
+  `title` VARCHAR(5000) NULL,
+  PRIMARY KEY (`url`))
+ENGINE = InnoDB;
+
 
 -- -----------------------------------------------------
 -- Table `mydb`.`playlist`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`playlist` (
-  `id` VARCHAR(60) NOT NULL,
+  `idplaylist` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(5000) NOT NULL,
-  `rank` INT NOT NULL,
+  `ranking` INT NOT NULL,
   `date` DATE NOT NULL,
   `artist` VARCHAR(1000) NOT NULL,
   `url` VARCHAR(60) NOT NULL,
   `region` VARCHAR(45) NOT NULL,
-  `chart` VARCHAR(15) NOT NULL,
+  `chart` VARCHAR(25) NOT NULL,
   `trend` VARCHAR(45) NOT NULL,
   `streams` DOUBLE NULL,
   `highest_rank` INT NOT NULL,
-  `weeks_in_charts` INT NOT NULL,
-  PRIMARY KEY (`id`))
+  `weeks_in_rank` INT NOT NULL,
+  `artist_url` VARCHAR(60) NOT NULL,
+  PRIMARY KEY (`idplaylist`),
+  INDEX `fk_playlist_artist1_idx` (`artist_url` ASC) VISIBLE,
+  CONSTRAINT `fk_playlist_artist1`
+    FOREIGN KEY (`artist_url`)
+    REFERENCES `mydb`.`artist` (`url`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -41,11 +59,13 @@ ENGINE = InnoDB;
 -- Table `mydb`.`lastfm`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`lastfm` (
-  `idlastfm` INT NOT NULL,
-  `published_track` VARCHAR(45) NULL,
-  `published` VARCHAR(45) NULL,
-  `summary` VARCHAR(2000) NULL,
-  `content` VARCHAR(5000) NULL,
+  `url` VARCHAR(60) NOT NULL,
+  `artist` VARCHAR(1000) NOT NULL,
+  `track` VARCHAR(5000) NOT NULL,
+  `published_track` DATETIME NULL,
+  `published` DATETIME NULL,
+  `summary` BLOB NULL,
+  `content` LONGBLOB NULL,
   `aritist_genre_0` VARCHAR(25) NULL,
   `aritist_genre_1` VARCHAR(25) NULL,
   `aritist_genre_2` VARCHAR(25) NULL,
@@ -56,12 +76,13 @@ CREATE TABLE IF NOT EXISTS `mydb`.`lastfm` (
   `track_genre_2` VARCHAR(25) NULL,
   `track_genre_3` VARCHAR(25) NULL,
   `track_genre_4` VARCHAR(25) NULL,
-  `url` VARCHAR(60) NOT NULL,
-  PRIMARY KEY (`idlastfm`),
-  INDEX `fk_lastfm_playlist1_idx` (`url` ASC) VISIBLE,
-  CONSTRAINT `fk_lastfm_playlist1`
-    FOREIGN KEY (`url`)
-    REFERENCES `mydb`.`playlist` (`id`)
+  `genre` VARCHAR(45) NULL,
+  `artist_url` VARCHAR(60) NOT NULL,
+  PRIMARY KEY (`url`),
+  INDEX `fk_lastfm_artist1_idx` (`artist_url` ASC) VISIBLE,
+  CONSTRAINT `fk_lastfm_artist1`
+    FOREIGN KEY (`artist_url`)
+    REFERENCES `mydb`.`artist` (`url`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -71,11 +92,11 @@ ENGINE = InnoDB;
 -- Table `mydb`.`spotify`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`spotify` (
-  `idspotify` INT NOT NULL,
-  `artist` VARCHAR(1000) NULL,
-  `track` VARCHAR(5000) NULL,
-  `uri` VARCHAR(60) NULL,
-  `dancebility` DOUBLE NULL,
+  `url` VARCHAR(60) NOT NULL,
+  `artist` VARCHAR(1000) NOT NULL,
+  `track` VARCHAR(5000) NOT NULL,
+  `URI` VARCHAR(45) NULL,
+  `daceability` DOUBLE NULL,
   `energy` DOUBLE NULL,
   `key` INT NULL,
   `loudness` DOUBLE NULL,
@@ -86,16 +107,14 @@ CREATE TABLE IF NOT EXISTS `mydb`.`spotify` (
   `liveness` DOUBLE NULL,
   `valence` DOUBLE NULL,
   `tempo` DOUBLE NULL,
-  `type` VARCHAR(45) NULL,
-  `track_href` VARCHAR(45) NULL,
-  `time_seg` DOUBLE NULL,
-  `time_min` INT NULL,
-  `url` VARCHAR(60) NOT NULL,
-  PRIMARY KEY (`idspotify`),
-  INDEX `fk_spotify_playlist_idx` (`url` ASC) VISIBLE,
-  CONSTRAINT `fk_spotify_playlist`
-    FOREIGN KEY (`url`)
-    REFERENCES `mydb`.`playlist` (`id`)
+  `duration_ms` DOUBLE NULL,
+  `duration_mins` INT NULL,
+  `artist_url` VARCHAR(60) NOT NULL,
+  PRIMARY KEY (`url`, `artist_url`),
+  INDEX `fk_spotify_artist1_idx` (`artist_url` ASC) VISIBLE,
+  CONSTRAINT `fk_spotify_artist1`
+    FOREIGN KEY (`artist_url`)
+    REFERENCES `mydb`.`artist` (`url`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
