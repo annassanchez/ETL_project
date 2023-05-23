7330dda10defdf3b41d708601f9b1c0b0e0cb49f
annassanchez <annassanchez@gmail.com>
2023-05-23 14:13:09 +0200

    changes added

--- src/soportecleaning.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from datetime import date
-
-def calculate_age(birthday):
-    today = date.today()
-    return today.year - birthday.year - ((today.month, today.day) < (birthday.month, birthday.day))
